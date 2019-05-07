df261a55e3e01c3abc6768fdf58b91eac1e91813
Rodrigo <rodrigobastosv@gmail.com>
2019-05-06 21:29:32 -0300

    Imports normalization

--- src/main/java/br/com/glad/gladtask/controllers/UserController.java
+++ src/main/java/br/com/glad/gladtask/controllers/UserController.java
@@ -22,9 +22,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
